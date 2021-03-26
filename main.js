2035ba8ae68cd0287905cd3f2bdb3e4dfbbd2499
ebdevStudent <78286626+ebdevStudent@users.noreply.github.com>
2021-03-25 21:07:48 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+
+// keyword length
+// keywords are the reserved words of a coding language
+
+var var1 = "Rahul Allala";
+var len = var1.length;
+console.log (len);
+// replace function (para1, para2)
+var name_1 = "hello I am Rahul Desgupta";
+var replace_1 = name_1.replace ("Desgupta","Allala");
+console.log(replace_1);
+
+// toLowerCase() function
+var capital_1 = "HELLO";
+var answer_1 = capital_1.toLowerCase();
+console.log (answer_1);
+
+var lower_1 = "salutations";
+var answer_2 = lower_1.toUpperCase();
+console.log (answer_2);
+
+// charAt(index number)
+// length = 11 (salutations)
+var print_u = lower_1.charAt(3);
+console.log(print_u);
+
+// print the last letter of a word
+var lt_1 = "Geronimo";
+var lastlt_1 = lt_1.charAt(7);
+console.log (lastlt_1);
+
+var replace_2 = lt_1.replace ("o","_");
+var replace_3 = replace_2.replace ("o","_");
+console.log (replace_2);
+console.log (replace_3);
